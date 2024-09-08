5d46985cacb87f7e05df715d15d6102f0803af06
Thaynoanhit <hitab2000@gmail.com>
2024-09-07 22:19:12 -0300

    feat: validação no FormsLogin

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,4 @@
-import FormLogin from "@/components/FormLogin";
-import "./page.css"
+import FormLogin from "@/components/FormLogin/FormLogin";
 export default function LoginPage() {
   return (
       <div>
