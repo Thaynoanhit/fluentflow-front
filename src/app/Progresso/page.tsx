ca1e304bbbb0c93ebd0c2bf9eaf27c13c69011c0
Elder <elderh.correia@gmailcom>
2024-09-08 18:24:33 -0300

    style: Alteração no layout da opção de Progresso

--- src/app/Progresso/page.tsx
+++ src/app/Progresso/page.tsx
@@ -2,13 +2,9 @@ import React from "react";
 import styles from "./page.module.css";
 import NavigationBar from "../../components/NavigationBar/NavigationBar";
 
-const HomePage: React.FC = () => {
+const ProgressoPage: React.FC = () => {
 
-// Valores estáticos
-
-  const userName = "Nome do Usuário"; 
-  const joinDate = "01/01/2024"; 
-
+  // Valores estáticos
   const palavrasAprendidas = 120;
   const palavrasEmRevisao = 45;
   const palavrasParaEstudo = 30;
@@ -16,74 +12,40 @@ const HomePage: React.FC = () => {
 
   return (
     <div className={styles.pageContainer}>
-      <div className={styles.profileContainer}>
-        <img
-          src="/icons/UserPerfil3.gif"
-          alt="Avatar"
-          className={styles.avatar}
-        />
-        <div className={styles.userInfo}>
-          <h2 className={styles.profileText}>Perfil</h2>
-          <p className={styles.userName}>{userName}</p>
-          <p className={styles.joinDate}>{joinDate}</p>
-        </div>
-      </div>
-
-      <hr className={styles.divider} />
-
-      <h1 className={styles.title}>Estatísticas</h1>
-
-      {/* Seção de palavras descobertas */}
-      <div className={styles.statsContainer}>
+      <h1 className={styles.title}>Estatísticas de Apendizagem</h1>
+      
+      <div className={styles.centralContainer}>
+        
         <div className={styles.statCard}>
-          <div className={styles.iconTextContainer}>
-            <img
-              src="/icons/PalavrasAprendidas.png"
-              alt="Palavras Aprendidas"
-              className={styles.statIcon}
-            />
-            <div className={styles.statText}>
-              <h3 className={styles.statTitle}>Palavras Aprendidas</h3>
-              <p className={styles.statValue}>{palavrasAprendidas}</p>
-            </div>
+          <img src="/icons/Palavras_Aprendidas.png" alt="Palavras Aprendidas" className={styles.statIcon} />
+          <div className={styles.statText}>
+            <h3 className={styles.statTitle}>Palavras Aprendidas</h3>
+            <p className={`${styles.statValue} ${styles.palavrasAprendidas}`}>{palavrasAprendidas}</p>
           </div>
         </div>
+
         <div className={styles.statCard}>
-          <div className={styles.iconTextContainer}>
-            <img
-              src="/icons/Revisar.png"
-              alt="Palavras em Revisão"
-              className={styles.statIcon}
-            />
-            <div className={styles.statText}>
-              <h3 className={styles.statTitle}>Palavras em Revisão</h3>
-              <p className={styles.statValue}>{palavrasEmRevisao}</p>
-            </div>
+          <img src="/icons/Palavras_Revisão.png" alt="Palavras em Revisão" className={styles.statIcon} />
+          <div className={styles.statText}>
+            <h3 className={styles.statTitle}>Palavras em Revisão</h3>
+            <p className={styles.statValue}>{palavrasEmRevisao}</p>
           </div>
         </div>
+
         <div className={styles.statCard}>
-          <div className={styles.iconTextContainer}>
-            <img
-              src="/icons/EstudarPalavra.png"
-              alt="Palavras para Estudo"
-              className={styles.statIcon}
-            />
-            <div className={styles.statText}>
-              <h3 className={styles.statTitle}>Palavras para Estudo</h3>
-              <p className={styles.statValue}>{palavrasParaEstudo}</p>
-            </div>
+          <img src="/icons/Palavras_Estudo.png" alt="Palavras para Estudo" className={styles.statIcon} />
+          <div className={styles.statText}>
+            <h3 className={styles.statTitle}>Palavras para Estudo</h3>
+            <p className={styles.statValue}>{palavrasParaEstudo}</p>
           </div>
         </div>
-      </div>
-      <div className={styles.totalContainer}>
-        <img
-          src="/icons/Total.png"
-          alt="Total"
-          className={styles.totalIcon}
-        />
-        <div className={styles.totalText}>
-          <h3 className={styles.totalTitle}>Total de Palavras</h3>
-          <p className={styles.totalValue}>{totalPalavras}</p>
+
+        <div className={styles.statCard}>
+          <img src="/icons/Palavras_Total.png" alt="Total de Palavras" className={styles.statIcon} />
+          <div className={styles.statText}>
+            <h3 className={styles.statTitle}>Total de Palavras</h3>
+            <p className={styles.statValue}>{totalPalavras}</p>
+          </div>
         </div>
       </div>
       <NavigationBar />
@@ -91,4 +53,4 @@ const HomePage: React.FC = () => {
   );
 };
 
-export default HomePage;
+export default ProgressoPage;
