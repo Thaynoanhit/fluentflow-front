8c1fc3bd29ab7e663029848fe374730411ba2c3e
Elias Lima <sci.eliaslima@gmail.com>
2024-09-07 21:11:50 -0300

    fix: change category list

--- src/components/CategoryList/CategoryList.tsx
+++ src/components/CategoryList/CategoryList.tsx
@@ -1,4 +1,4 @@
-import styles from "@/components/CategoryList.module.css";
+import styles from './CategoryList.module.css'
 
 function CategoryList({categories}: any) {
     return(
@@ -6,11 +6,7 @@ function CategoryList({categories}: any) {
             <h1>Escolha uma lista de palavras</h1>
             {categories.map((c: any) => (
                 <div key={c.id} className={styles.category}>
-                    
                     <a href={`/palavras/categoria/${c.id}`}>{c.categoryName}</a>
-                    <span
-                     style={{float:"right", fontSize:"30px"}}
-                     className="material-symbols-outlined">expand_circle_right</span>
                      <p className={styles.percentage}>50%</p>
                      <p className={styles.totalWords}>2870 palavras</p>
                 </div>
