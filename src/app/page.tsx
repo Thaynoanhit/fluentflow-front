ffd081e4e9c122dd185cdb9f5cdb7b3badda249c
Elias Lima <sci.eliaslima@gmail.com>
2024-09-09 13:15:42 -0300

    feat: create home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,48 @@
 'use client'
+import Image from "next/image";
+import { useRouter } from "next/navigation";
+
+import { 
+  HeaderContainer, 
+  HeaderInnerContainer, 
+  FluentFlowBrand, 
+  FluentFlowBannerWrapper,
+  ButtonsWrapper,
+  RegisterButton,
+  BannerTitle,
+  HaveAccountButton
+} from "./styles";
+import FluentFlowBanner from '../assets/img/banner-fluentflow.png';
 
 export default function Home() {
+
+  const router = useRouter();
+
+  const handleNavigate = (route:string) => {
+    router.push(route)
+  }
+
   return (
     <>
-      <p> BRAND</p>
+      <HeaderContainer>
+        <HeaderInnerContainer>
+          <FluentFlowBrand>FluentFlow</FluentFlowBrand>
+        </HeaderInnerContainer>
+      </HeaderContainer>
+      <FluentFlowBannerWrapper>
+        <Image src={FluentFlowBanner} width={400} />
+        <ButtonsWrapper>
+          <BannerTitle>
+            O Jeito rápido e fácil de aprender um idioma!
+          </BannerTitle>
+          <RegisterButton onClick={() => handleNavigate('register')}>
+            Criar uma conta
+          </RegisterButton>
+          <HaveAccountButton onClick={() => handleNavigate('login')}>
+            Já tenho uma conta
+          </HaveAccountButton>
+        </ButtonsWrapper>
+      </FluentFlowBannerWrapper>
     </>  
   );
 }
